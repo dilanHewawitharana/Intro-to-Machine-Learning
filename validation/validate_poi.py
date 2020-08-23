b7f3622317f06554c6aaf3f63fd1ac557199a589
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2020-08-23 09:48:37 +0900

    Completed code for validation lessons

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -27,6 +27,18 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### it's all yours from here forward!  
+### it's all yours from here forward! 
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print('Accuracy: {}'.format(accuracy))
 
 
